ed14fb0fb22e4596a9e354feab8b3e06c3be5f6b
Victor Holt <vholt@dhcp-206.kc.bravo>
2014-07-24 16:45:04 -0500

    Started building out the base files.

---
+++ build-config-dist.js
@@ -0,0 +1,38 @@
+/**
+ * Build configuration constants.
+ *
+ * This file contains all of the build configurations which are used
+ * when compiled by Grunt.
+ *
+ * @class BuildConfig
+ * @static
+ */
+
+var BuildConfig = {
+    /**
+     * The grunt banner that goes above all
+     * generated files.
+     *
+     * @property GRUNT_BANNER
+     * @type String
+     */
+    GRUNT_BANNER =  [
+        'THIS IS A GENERATED FILE AND SHOULD NOT BE EDITTED!',
+    ],
+
+    /**
+     * The build source diretory.
+     *
+     * @property DIR_SRC
+     * @type String
+     */
+    DIR_SRC = './src',
+
+    /**
+     * The build destination directory.
+     *
+     * @property DIR_BUILD
+     * @type String
+     */
+    DIR_DEST = './web'
+};
