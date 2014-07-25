874d75d4d0eaa721d4eb8f03452615003304871d
Victor Holt <vqholt@gmail.com>
2014-07-25 16:58:08 -0500

    Started working on the simple node server

---
+++ tools/node-server/node-server.js
@@ -0,0 +1,7 @@
+/* jshint node:true, laxbreak: true */
+'use strict';
+
+/**
+ * A simple node server for testing the application. A more roboust
+ * application may want to add the server-side code in the /src folder.
+ */
